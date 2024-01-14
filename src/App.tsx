d0657c905a677042c8456fcef4e7cd3d08a1adc4
Jordan Baron <jordannbaron@gmail.com>
2024-01-14 13:09:34 -0500

    solid + tw setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { createSignal, onCleanup } from "solid-js";
+
+export function App() {
+  const [count, setCount] = createSignal(0),
+    timer = setInterval(() => setCount(count() + 1), 1000);
+  onCleanup(() => clearInterval(timer));
+
+  return <div>{count()}</div>;
+}
