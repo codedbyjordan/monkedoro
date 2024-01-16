24817c0ed6dac76005a79717400bf91dd6c61232
Jordan Baron <jordannbaron@gmail.com>
2024-01-15 19:02:12 -0500

    button changes

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,15 +2,17 @@ import { VariantProps, cva } from "class-variance-authority";
 import { ComponentProps } from "solid-js";
 import { twMerge } from "tailwind-merge";
 
-const buttonStyles = cva("px-3 py-2 w-full rounded-md transition-colors", {
-  variants: {
-    variant: {
-      primary:
-        "bg-neutral-800 hover:bg-neutral-700 text-white font-semibold  flex justify-center",
-      stroked: "text-white border-2 hover:bg-stone-600/50",
+const buttonStyles = cva(
+  "px-3 py-2 w-full rounded-md transition-colors flex justify-center items-center",
+  {
+    variants: {
+      variant: {
+        primary: "bg-neutral-800 hover:bg-neutral-700 text-white font-semibold",
+        stroked: "text-white border-2 hover:bg-stone-600/50",
+      },
     },
-  },
-});
+  }
+);
 
 type ButtonProps = VariantProps<typeof buttonStyles> & ComponentProps<"button">;
 
